c1572f8577c178ce04b632b442fb58d26080947f
Екатерина <diada.var@mail.ru>
2024-09-21 17:42:35 +0300

    исправленый 1

---
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -0,0 +1,43 @@
+package pro.sky.calculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+    private final CalculatorSevise service;
+
+    public CalculatorController(CalculatorSevise service) {
+        this.service = service;
+    }
+    @GetMapping
+    public String hello() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + " + " + num2 + " = " + service.plus(num1, num2);
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + " - " + num2 + " = " + service.minus(num1, num2);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + " / " + num2 + " = " + service.divide(num1, num2);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam Integer num1, @RequestParam Integer num2) {
+        return num1 + " * " + num2 + " = " + service.multiply(num1, num2);
+    }
+}
---
+++ src/main/java/pro/sky/calculator/CalculatorSevise.java
@@ -0,0 +1,22 @@
+package pro.sky.calculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorSevise {
+
+
+    public Integer plus(Integer num1, Integer num2) {
+        return num1+ num2;
+    }
+    public Integer minus (Integer num1, Integer num2) {
+        return num1-num2;
+    }
+    public Double divide(Integer num1, Integer num2) {
+        return (double) num1/num2;
+    }
+
+    public Integer multiply(Integer num1, Integer num2) {
+        return num1*num2;
+    }
+}
